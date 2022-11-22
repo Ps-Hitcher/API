ddaed30a4bdba871c5ab0b1db4bf41e99e5c7367
Domasen <domas.nemanius@gmail.com>
2022-11-22 23:41:21 +0200

    fix

--- API/Controllers/HomeController.cs
+++ API/Controllers/HomeController.cs
@@ -40,8 +40,6 @@ public class HomeController : Controller
         _travelList = _travelRepository.GetTravelList();//debug
     }
 
-
-    public MockUserRepository rep = new MockUserRepository();
     
     [HttpGet]
     [Route("test/PrintMessage")]
