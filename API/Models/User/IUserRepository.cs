e3ed9b659f113f1c3f0be3ce089f4c867349c1d6
Samusiovaite <kamilesamusiovaite@gmail.com>
2022-12-11 11:35:15 +0200

    Task-48-Tests

--- API/Models/User/IUserRepository.cs
+++ API/Models/User/IUserRepository.cs
@@ -2,7 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 public interface IUserRepository
 {
-    UserModel GetUser(Guid Id);
+    public UserModel GetUser(Guid Id);
     DbSet<UserModel> GetUserList();
     public bool IsValidPhone(string PhoneNumber);
     public void DeleteUser(Guid Id);
